af479f8613ad15ddc0434ec64d03ddce85e72041
Joashhh <129048031+Joashhh@users.noreply.github.com>
2025-01-27 16:43:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ window.geometry("420x420")
 window.title("CCT)
 window.config(background="#e0f9ff")
 window.mainloop()
+flag = "flag"
              
 def confirm_action():
     messagebox.showinfo("Confirmed")
+
+def flag_input():
+  user_input = flag_textbox.get("1.0", tk.END).strip()
+   if user_input:
+        if user_input == flag: 
+             messagebox.showinfo("Flag", "Correct flag entered!")
+        else:
+            messagebox.showerror("Flag", "Incorrect flag. Try again.")
