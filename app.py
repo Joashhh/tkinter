94c677e7ee56b504bc83d41eae8ebf0b2d00dbad
Joashhh <129048031+Joashhh@users.noreply.github.com>
2025-01-27 16:45:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ window.mainloop()
 flag = "flag"
              
 def confirm_action():
-    messagebox.showinfo("Confirmed")
+  messagebox.showinfo("Confirmed")
 
 def flag_input():
   user_input = flag_textbox.get("1.0", tk.END).strip()
-   if user_input:
-        if user_input == flag: 
-             messagebox.showinfo("Flag", "Correct flag entered!")
-        else:
-            messagebox.showerror("Flag", "Incorrect flag. Try again.")
+  if user_input:
+    if user_input == flag:
+      messagebox.showinfo("Flag", "Correct flag entered!")
+    else:
+      messagebox.showerror("Flag", "Incorrect flag. Try again.")
